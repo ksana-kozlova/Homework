0a7f6e6e7e064a7f3eea94bf913187511f4eadf7
kozlova_oksana <kozlova.ksucha@yandex.ru>
2020-02-25 18:21:47 +0300

    Lesson-9

--- Lesson-9/third.sql
+++ Lesson-9/third.sql
@@ -8,11 +8,6 @@ CREATE TABLE IF NOT EXISTS employees (
     salary INT UNSIGNED NOT NULL
     
 );
-INSERT INTO employees ( id, first_name, last_name, position, salary) VALUES ( null, 'Zhelezin', 'Mikhail', 'Deep Learning Software Engineer', '40000');
-INSERT INTO employees ( id, first_name, last_name, position, salary) VALUES ( null, 'Kozlova', 'Oksana', 'Programmig manager', '25000');
-INSERT INTO employees ( id, first_name, last_name, position, salary) VALUES ( null, 'Malysheva', 'Ekaterina', 'Technical Writer', '10000');
-INSERT INTO employees ( id, first_name, last_name, position, salary) VALUES ( null, 'Pyankova', 'Ekaterina', 'Entertainer', '15000');
-INSERT INTO employees ( id, first_name, last_name, position, salary) VALUES ( null, 'Yastrebova', 'Oksana', 'Technical Writer', '15000');
 
 CREATE TABLE IF NOT EXISTS service_relationships (
 	head_id INT UNSIGNED NOT NULL,
@@ -21,12 +16,19 @@ CREATE TABLE IF NOT EXISTS service_relationships (
     FOREIGN KEY (head_id) REFERENCES employees(id)
 );
 
+INSERT INTO employees ( id, first_name, last_name, position, salary) VALUES ( null, 'Zhelezin', 'Mikhail', 'Deep Learning Software Engineer', '40000');
+INSERT INTO employees ( id, first_name, last_name, position, salary) VALUES ( null, 'Kozlova', 'Oksana', 'Programmig manager', '25000');
+INSERT INTO employees ( id, first_name, last_name, position, salary) VALUES ( null, 'Malysheva', 'Ekaterina', 'Technical Writer', '10000');
+INSERT INTO employees ( id, first_name, last_name, position, salary) VALUES ( null, 'Pyankova', 'Ekaterina', 'Entertainer', '15000');
+INSERT INTO employees ( id, first_name, last_name, position, salary) VALUES ( null, 'Yastrebova', 'Oksana', 'Technical Writer', '15000');
+
 INSERT INTO service_relationships (head_id, subordinate_id) VALUES (1, 2);
 INSERT INTO service_relationships (head_id, subordinate_id) VALUES (1, 3);
 INSERT INTO service_relationships (head_id, subordinate_id) VALUES (4, 1);
 INSERT INTO service_relationships (head_id, subordinate_id) VALUES (5, 4);
 
-SELECT * FROM staff.employees employee
-INNER JOIN service_relationships relationship ON relationship.subordinate_id=employee.id;
+
+SELECT * FROM (employees employee INNER JOIN service_relationships relationship ON relationship.subordinate_id=employee.id)
+JOIN employees employee2 ON head_id=employee2.id WHERE employee2.last_name='Zhelezin';
 
 
